1fd6e2a207e14fa4d58c571bdae9ec82c60666a1
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-09-05 14:15:22 -0700

    updated

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,21 +1,6 @@
-// const languages = ['C is fun','Python is cool','JavaScript is amazing'];
+const languages = ['C is fun','Python is cool','JavaScript is amazing'];
 
-// // using for loop to iterate the array
-// for(const language of languages){
-//     console.log(language);
-// }
-
-const lines = [
-    "C is fun",
-    "Python is cool",
-    "JavaScript is amazing"
-  ];
-  
-  function printLines(index) {
-    if (index < lines.length) {
-      console.log(lines[index]);
-      printLines(index + 1);
-    }
-  }
-  
-  printLines(0);
+// using for loop to iterate the array
+for(const language of languages){
+    console.log(language);
+}
