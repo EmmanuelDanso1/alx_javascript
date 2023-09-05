137c26471a3641f20994c002e2f5c8b43a8bccf5
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-09-05 09:54:18 -0700

    loops to iterate

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+const threeLines = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+];
+
+// using for loop to iterate the array
+for(let i = 0; i < threeLines.length; i++){
+    console.log(threeLines[i]);
+}
