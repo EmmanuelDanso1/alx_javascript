e100437610861413fbbac46e47d4aa7b95cab975
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-09-07 06:22:48 -0700

    if w or h is equal to 0 or not a positive integer, create an empty object

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+class Rectangle{
+    constructor(w, h){
+        if(w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+        // checking f w or h is equal to 0 or not a positive integer, create an empty object
+        else{
+            this.width = undefined;
+            this.height = undefined;
+        }
+    }
+}
+
+module.exports = Rectangle;
