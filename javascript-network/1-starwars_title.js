77af503a8834263c14d0542c3115dac88d6b78ec
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-09-12 22:19:07 -0700

    new commit

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -9,10 +9,21 @@ if (process.argv.length !== 3) {
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-requests.get(url, {encoding:'utf-8'}).on('data', (data) =>{
-    //  console.log(data)
-    const object = JSON.parse(data)
-     console.log(object.title);
-    
-})
- 
+requests.get(url, (error, response, data) => {
+  if (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: Status code ${response.statusCode}`);
+    process.exit(1);
+  }
+  try {
+    const object = JSON.parse(data);
+    console.log(object.title);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError.message);
+    process.exit(1);
+  }
+});
