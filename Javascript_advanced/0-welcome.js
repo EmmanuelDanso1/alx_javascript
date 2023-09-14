302958e65c924fab888343390e9341767e555a68
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-09-14 03:13:03 -0700

    function and alert

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//  function that takes two argument and display with alert keyword
+
+function welcome(firstName, lastName){
+    // declaring a variable 
+    const fullName = `${firstName} ${lastName}`;
+    
+    function displayFullName(){
+        alert(`welcome ${fullName}!`)
+    }
+    displayFullName()
+}
+// calling the welcome function in order to execute the welcome argument
+welcome();
